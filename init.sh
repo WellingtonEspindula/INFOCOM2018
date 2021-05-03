03520e047bb8050fc3cbf43b390b8cf8f30be4ab
Wellington Espindula <wellington.m.espindula@gmail.com>
2021-05-03 16:37:30 -0300

    Add init.sh script

---
+++ init.sh
@@ -0,0 +1,11 @@
+#/usr/bin/env bash
+
+echo "[INFO] Cleaning previous mininet config..."
+sudo mn -c
+clear
+echo "[INFO] Initializing controller..."
+sudo ryu-manager Controller_DBR.py &
+sleep 3s
+clear
+echo "[INFO] Initializing Topology..."
+sudo python3 Topo_DBR.py
