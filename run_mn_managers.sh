67f7b4f14008f41fb493da5e975c8214ff6bd333
Wellington Espindula <wellington.m.espindula@gmail.com>
2021-05-04 09:32:12 -0300

    Changed CDNs to Managers and all are moved to core. Add script to run Netmetric Managers.

---
+++ run_mn_managers.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+$m man1 /usr/netmetric/sbin/metricmanager &
+$m man2 /usr/netmetric/sbin/metricmanager &
+$m man3 /usr/netmetric/sbin/metricmanager &
+$m man4 /usr/netmetric/sbin/metricmanager &
