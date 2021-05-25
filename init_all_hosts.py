6fc6627d65078f331c8a4784989f88e62d7c59f4
Wellington Espindula <wellington.m.espindula@gmail.com>
2021-05-25 15:37:08 -0300

    Hosts initialization and temporal random

--- init_all_hosts.py
+++ init_all_hosts.py
@@ -6,3 +6,4 @@ for i in range(1, 201):
     hostname = f"u{i:03}"
     url = f"http://192.168.15.148:8080/bqoepath/admweights-{hostname}-all"
     os.system(f'curl -q -s {url}')
+    os.system(f'./temporal_randomizer.py 5 {hostname} man1')
