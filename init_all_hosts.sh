b43eb4f1c5cb027e7c70e1a6c0551ee916948041
Wellington Espindula <wellington.m.espindula@gmail.com>
2021-05-25 22:20:23 -0300

    Improvements on temporal_randomizer.py and created init_all_hosts.sh script

---
+++ init_all_hosts.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+host="192.168.15.148"
+port=8080
+
+for i in {001..201}; do
+  # Creates hostname from iteration (u001-u201) and create the url to build the path
+  hostname="u${i}"
+  url="http://${host}:${port}/bqoepath/admweights-${hostname}-all"
+
+  # Calls the URL by GET using curl to create the path
+  output=$(curl -q -s $url)
+
+  # Get the destination info from curl's output
+  destination=$(echo "$output" | jq -r ".dst")
+#  destination_ip=$(echo "$output" | jq -r ".dest_ip")
+
+  ./temporal_randomizer.py -f 5 "$hostname" "$destination"
+done
--- temporal_randomizer.py
+++ temporal_randomizer.py
@@ -3,15 +3,11 @@
 import csv
 import os
 import shutil
-import sys
 import time
 import uuid
 import xml.etree.ElementTree as et
 from datetime import datetime
-
-period = int(sys.argv[1]) * 60
-hostname = sys.argv[2]
-manager = sys.argv[3]
+from argparse import ArgumentParser
 
 
 def random():
@@ -20,14 +16,14 @@ def random():
 
 def read_xml(filename):
     root = et.parse(filename).getroot()
-    upmin = root.find("./ativas/upmin").text
-    upmax = root.find("./ativas/upmax").text
-    upavg = root.find("./ativas/upavg").text
-    downmin = root.find("./ativas/downmin").text
-    downmax = root.find("./ativas/downmax").text
-    downavg = root.find("./ativas/downavg").text
+    upload_min = root.find("./ativas/upmin").text
+    upload_max = root.find("./ativas/upmax").text
+    upload_avg = root.find("./ativas/upavg").text
+    download_min = root.find("./ativas/downmin").text
+    download_max = root.find("./ativas/downmax").text
+    download_avg = root.find("./ativas/downavg").text
     status = root.find("./ativas/status").text
-    return [upmin, upmax, upavg, downmin, downmax, downavg, status]
+    return [upload_min, upload_max, upload_avg, download_min, download_max, download_avg, status]
 
 
 def write_data_csv(filename, data):
@@ -37,23 +33,44 @@ def write_data_csv(filename, data):
         # print("File saved")
 
 
-time_to_run = ((60 * (random() % 10)) + (30 + (random() % 29)))
-# time_to_run = (3 + (random() % 29))
-print(f"{time_to_run}s")
-time.sleep(time_to_run)
+if __name__ == '__main__':
+    # Informing script arguments
+    parser = ArgumentParser(description='Runs the metricagent using a random trigger time')
+    parser.add_argument("-f", "--fast", help="fast initial trigger", action="store_true")
+    parser.add_argument("-v", "--verbose", help="verbose mode", action="store_true")
+    parser.add_argument("period", type=int, help="Trigger period in minutes")
+    parser.add_argument("hostname", type=str, help="Agent hostname")
+    parser.add_argument("manager", type=str, help="Manager hostname")
+    args = parser.parse_args()
 
-while True:
-    # print(script)
-    # print(period)
-    sch_uuid = str(uuid.uuid4())
-    script = f"/usr/netmetric/sbin/metricagent -c -f schedules/agenda-{manager}.xml -w -l 1000 -u 100 -u {sch_uuid}"
-    os.system(script)
+    # Read parameters from input
+    period = args.period * 60
+    hostname = args.hostname
+    manager = args.manager
 
-    current_timestamp = str(datetime.now())
-    data = [hostname, manager, current_timestamp, sch_uuid]
-    data.extend(read_xml(f"agent-{sch_uuid}.xml"))
-    print(data)
-    write_data_csv("results/load.csv", data)
-    shutil.move(f"agent-{sch_uuid}.xml", f"./results/xml/agent-{sch_uuid}.xml")
+    # Time to run depends on it's running or fast first trigger mode
+    if args.fast:
+        first_trigger_time = (3 + (random() % 29))
+    else:
+        first_trigger_time = ((60 * (random() % 10)) + (30 + (random() % 29)))
 
-    time.sleep(period)
+    print(f"Running in {first_trigger_time} s")
+    time.sleep(first_trigger_time)
+
+    # Keep waiting the given period and calls metricagent
+    while True:
+        # Regenerate UUID from schedule
+        sch_uuid = str(uuid.uuid4())
+        # Creates the metric agent command
+        command = f"/usr/netmetric/sbin/metricagent -c -f schedules/agenda-{manager}.xml -w -l 1000 -u 100 -u {sch_uuid}"
+        os.system(command)
+
+        # Gather the data from metricagent xml output
+        current_timestamp = str(datetime.now())
+        data = [hostname, manager, current_timestamp, sch_uuid]
+        data.extend(read_xml(f"agent-{sch_uuid}.xml"))
+        write_data_csv("results/load.csv", data)
+        # Moves the XML file from results/xml folder
+        shutil.move(f"agent-{sch_uuid}.xml", f"./results/xml/agent-{sch_uuid}.xml")
+
+        time.sleep(period)
